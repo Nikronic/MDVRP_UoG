6a4aa7ea6809eabed16b5a41021c3c818af1f1c6
NIkan <ad2@sad.AS>
2017-12-11 23:25:12 +0330

    Adding project

---
+++ Program.cs/MDVRP_ORIG/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDVRP_ORIG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
