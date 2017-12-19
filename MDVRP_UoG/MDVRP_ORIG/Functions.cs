c5217e2acde201ac9f35fe0583b068b3a835eb1d
NIkan <ad2@sad.AS>
2017-12-19 03:31:03 +0330

    Tournament

--- MDVRP_UoG/MDVRP_ORIG/Functions.cs
+++ MDVRP_UoG/MDVRP_ORIG/Functions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -98,10 +100,85 @@ namespace MDVRP_ORIG
         }
 
 
-        public static List<Chromosome> Tournament(List<Chromosome> population)
+        public static List<Chromosome> Tournament(List<Chromosome> population,int populationSize)
         {
+            List<Chromosome> tournamentOutput = new List<Chromosome>();
 
-            return null;
+            List<Chromosome> tournamentSet = new List<Chromosome>();
+            tournamentSet = TournamentPopulation(population, populationSize);
+            const double probabilityOfTournament = 0.8f; // the parameter of paper
+            Random random = new Random();
+            int randomProb = random.Next();
+            if (randomProb <= probabilityOfTournament)
+            {
+                List<Chromosome> tournamentSet2 = new List<Chromosome>();
+                tournamentSet2 = TournamentPopulation(population, populationSize);
+
+                // get to fittest
+                Chromosome firstFittest = new Chromosome(-1,-1);
+                firstFittest = FittestChromosome(tournamentSet);
+                Chromosome secondFittest = new Chromosome(-1,-1);
+                secondFittest = FittestChromosome(tournamentSet2);
+
+                // output to crossover
+                tournamentOutput.Add(firstFittest);
+                tournamentOutput.Add(secondFittest);
+            }
+            else
+            {
+                int randomChromosome = random.Next(0, tournamentSet.Count);
+                Chromosome firstChromosome = new Chromosome(-1,-1);
+                firstChromosome = tournamentSet[randomChromosome];
+
+                int randomChromosome2 = random.Next(0, tournamentSet.Count);
+                Chromosome secondChromosome = new Chromosome(-1, -1);
+                secondChromosome = tournamentSet[randomChromosome];
+
+                // output to crossover
+                tournamentOutput.Add(firstChromosome);
+                tournamentOutput.Add(secondChromosome);
+
+            }
+            return tournamentOutput;
+        }
+
+        /// <summary>
+        /// We get a tournament set of chromosomes to select the fittest.
+        /// </summary>
+        /// <param name="k">Size of each tournament set</param>
+        /// <param name="population">Tournament set</param>
+        /// <returns></returns>
+        private static List<Chromosome> TournamentPopulation(List<Chromosome> population ,int k)
+        {
+            Random randomGenerator = new Random();
+            List<Chromosome> populationSet = new List<Chromosome>();
+            while (populationSet.Count != k)
+            {
+                int randomNumber = randomGenerator.Next(0, population.Count);
+                if (!populationSet.Contains(population[randomNumber]))
+                {
+                    populationSet.Add(population[randomNumber]);
+                }
+            }
+            return populationSet;
+        }
+
+        /// <summary>
+        /// Return the fittest chromosome in the tournament set.
+        /// </summary>
+        /// <param name="tournamentSet"></param>
+        /// <returns></returns>
+        private static Chromosome FittestChromosome(List<Chromosome> tournamentSet)
+        {
+            Chromosome fittest = new Chromosome(-1,-1);
+            foreach (Chromosome chromosome in tournamentSet)
+            {
+                if (chromosome.Fitness>= fittest.Fitness)
+                {
+                    fittest = chromosome;
+                }
+            }
+            return fittest;
         }
     }
 }
